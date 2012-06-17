4da1c5a0a76b78fe5fbe6d434784b2fa072bde36
Matt Mueller <mattmuelle@gmail.com>
2012-06-16 21:34:50 -0700

    sourceURL working, trying to get node_modules to work

--- example/ui/a/a.js
+++ example/ui/a/a.js
@@ -1,7 +1,10 @@
 require('./test.css');
-console.log(require('./blah.coffee'));
+console.log(require('../b/hello.mu'));
+// console.log(require('./blah.coffee'));
 
-var person = require('./person.json');
-console.log(person);
-module.exports = require('/models/model.js');
+// var person = require('./person.json');
+// console.log(person);
+// require('../b/css.styl');
+// console.log(require('/ui/b/b.coffee'));
+// module.exports = require('/models/model.js');
 
